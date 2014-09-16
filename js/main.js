7a3803a6dbcaa1d154bc89b54325ebf2bef33511
user <user@lubuntu.(none)>
2014-09-16 18:05:02 -0400

    Added jersey bg to modals, started to fix small screen animations that are screwy

--- js/main.js
+++ js/main.js
@@ -2,32 +2,30 @@
 $('.age-calc').on('click',function(event) {
 	event.preventDefault();
 
-	var gameday  = new Date(2003, 10, 1),
-		month     = ($('#bMonth').val()),
-		day       = $('#bDay').val(),
-		year      = $('#bYear').val(),
-		birthdate = new Date(year, month, day),
-		yourAge   = gameday - birthdate,
-		ageFormat = parseInt(yourAge / 31556900000);
+	var gameday    = new Date(2003, 10, 1),
+		month      = ($('#bMonth').val()),
+		day        = $('#bDay').val(),
+		year       = $('#bYear').val(),
+		birthdate  = new Date(year, month, day),
+		yourAge    = gameday - birthdate,
+		ageFormat  = parseInt(yourAge / 31556900000);
 
 	function hideForm(){
 		$('.form-group, .age-calc, .modal-title, .close').fadeOut(500);
 	}
 
-	//Add a puddles spinner, gets larger as emerges from center spinner runs for 1-2 seconds
-
-	//Reset the show/hide after the modal is closed
-
 	if(yourAge < 0) {
-		hideForm();
-		$('.notBorn').fadeIn(1500);
-		$('.shareResult').fadeIn(1500);
+		$('.form-group, .age-calc, .modal-title, .close').fadeOut(500, function(){
+			$('.notBorn').fadeIn(1500);
+			$('.shareResult').fadeIn(1500);
+		});
 	}
 	else {
 		hideForm();
-		$('.youWere').fadeIn(1500);  
-		$('.youWere span').append(ageFormat);
-		$('.shareResult').fadeIn(1500);
+		$('.youWere').fadeIn(1500, function(){
+			$('.youWere span').append(ageFormat);
+			$('.shareResult').fadeIn(1500);	
+		});  
 	}
 });
 
@@ -47,20 +45,60 @@ $('.facebook').on('click', function(event){
 	window.location.href = url;
 });
 
-//SHOW CREDITS
+//ANIMATE CONTAINER SIZE FOR CREDITS
+function creditsOpen() {
+	var winWidth   = $(window).width(),
+		mBody      = $('.modal-body'),
+		currHeight = mBody.height();
+
+	console.log(winWidth);
+
+	function openFades(){
+		$('.view_credits').fadeOut(200, function(){
+				$('.hide_credits').fadeIn(500);
+				$('#credits').fadeIn(500);	
+			});
+	}
+
+	if(winWidth > 384){
+		
+		var setHeight  = currHeight + 70;
+		mBody.css('height', setHeight);
+		mBody.animate({
+			height: '550'
+		}, 500, function(){
+			openFades();
+		});
+	}
+	else {
+		var setHeight  = currHeight + 70;
+		mBody.css('height', setHeight);
+		$('.view_credits').css('margin-bottom','0')
+		mBody.animate({
+			height: '600'
+		}, 500, function(){
+			openFades();
+		});
+	}
+	
+}
+
+function creditsClose() {
+	$('#credits').fadeOut(500, function(){
+		$('.modal-body').animate({
+			height: '400'
+		},500, function(){
+			$('.view_credits').fadeIn(350);
+		});
+	});
+}
+
 $('.view_credits').on('click', function(event){
 	event.preventDefault();
-	$('#credits').fadeIn(1500);
-	$(this).hide();
-	$('.hide_credits').show();
+	creditsOpen();
 });
 
-//HIDE CREDITS
 $('.hide_credits').on('click', function(event){
 	event.preventDefault();
-	$('#credits').fadeOut(1500);
-	$('.view_credits').fadeIn(1500);
-	$(this).hide();
-})
-
-//ADD JSCROLLPANE FOR PRETTIER SCROLLBARS
+	creditsClose();	
+});
