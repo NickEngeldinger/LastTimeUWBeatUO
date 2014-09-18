b543928ae98106487c062f1820a804ef546ae6bb
user <user@lubuntu.(none)>
2014-09-18 01:12:23 -0400

    Worked on cleaning up css, refactored js modal transitions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-//ANIMATE CONTAINER SIZE CHANGES
+//ANIMATE CREDITS SHOW/HIDE
 function creditsOpen() {
-	var winWidth    = $(window).width(),
+	$('.view_credits').hide();
+	$('#credits').show();
+	/*var winWidth    = $(window).width(),
 		mBody       = $('.modal-body'),
 		currHeight  = mBody.height(),
 		creditsLink = $('.view_credits');
@@ -30,58 +32,75 @@ function creditsOpen() {
 		}, 500, function(){
 			openFades();
 		});
-	}
+	}*/
 }
 
 function creditsClose() {
-	$('#credits').fadeOut(500, function(){
+	/*$('#credits').fadeOut(500, function(){
 		$('.modal-body').animate({
 			height: '400'
 		},500, function(){
 			$('.view_credits').fadeIn(350);
 		});
-	});
+	});*/
 }
 
 //CALCULATE A VISTORS AGE ON THE CALCULATOR PAGE AND DISPLAY IT
 function calculateAge(){
-	var calc = $('form');
+	var calc      = $('form'),
+		modal     = $('.modal-body'),
+		mHeight   = modal.height() + 70,
+		gameday   = new Date(2003, 10, 1),
+		month     = ($('#bMonth').val()),
+		day       = $('#bDay').val(),
+		year      = $('#bYear').val(),
+		birthdate = new Date(year, month, day),
+		yourAge   = gameday - birthdate,
+		formatAge = parseInt(yourAge / 31556900000);
+	
+	modal.css('height', mHeight);
 
-	$('.modal-title').fadeOut(500);
+	function showBtns(fadeTarget){
+		$('.shareResult').fadeIn(1500);
+		fadeTarget.fadeIn(1500);
+	}
 
-	calc.fadeOut(500, function(){
-
-		var gameday    = new Date(2003, 10, 1),
-			month      = ($('#bMonth').val()),
-			day        = $('#bDay').val(),
-			year       = $('#bYear').val(),
-			birthdate  = new Date(year, month, day),
-			yourAge    = gameday - birthdate,
-			ageFormat  = parseInt(yourAge / 31556900000);
-
-		if(yourAge < 0) {
+	function showAgeMsg(){
+		if(yourAge < 1) {
 			calc.fadeOut(500, function(){
-				$('.notBorn').fadeIn(1500);
-				$('.shareResult').fadeIn(1500);
+				var el = $('.notBorn');
+				showBtns(el);
 			});
 		}
 		else {
-			calc.fadeOut(500);
-			$('.youWere').fadeIn(1500, function(){
-				$('.youWere span').append(ageFormat);
-				$('.shareResult').fadeIn(1500);	
+			calc.fadeOut(500, function(){
+				var el = $('.youWere');
+				showBtns(el);
+				$('.youWere span').append(formatAge);
 			});  
 		}
-	});
+	}
 
-	
+	calc.fadeOut(500, function(){
+		modal.animate({
+			height: '399'
+		}, 500, function(){
+			showAgeMsg();
+		});
+	});
 }
 
-/////////////////////////////////////////////////////////////////////
-//
-// EVENT HANDLERS
-//
-////////////////////////////////////////////////////////////////////
+function connectTwitter(){
+	var age = $('.youWere span').html(),
+		url = ('twitter/redirect.php?age=' + age);
+	window.location.href = url;
+}
+
+function connectFacebook(){
+	var age = $('.youWere span').html(),
+		url = ('facebook/fb_login.php');
+	window.location.href = url;
+}
 
 $('.age-calc').on('click',function(event) {
 	event.preventDefault();
@@ -100,15 +119,11 @@ $('.hide_credits').on('click', function(event){
 
 $('.sendTweet').on('click',function(event) {
 	event.preventDefault();
-	var age = $('.youWere span').html(),
-		url = ('twitter/redirect.php?age=' + age);
-	window.location.href = url;
+	connectTwitter();
 });
 
 $('.facebook').on('click', function(event){
 	event.preventDefault();
-	var age = $('.youWere span').html(),
-		url = ('facebook/fb_login.php');
-	window.location.href = url;
+	connectFacebook();
 });
 
