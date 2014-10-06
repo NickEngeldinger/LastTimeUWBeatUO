51ce307444bcd0f9e815a161ac06e68eaca61540
user <user@lubuntu.(none)>
2014-10-06 16:00:39 -0400

    Added get variable for age

--- facebook/redirect.php
+++ facebook/redirect.php
@@ -1,6 +1,10 @@
 <?php
 
-/*THESE ARE HARDCODED TO POST TO MY PAGE ONLY, NEED TO CAPTURE FROM AUTH FLOW*/
+$age = $_GET['age'];
+echo $age;
+/*Check for no age and then set up message variable for you weren' even born yet*/
+
+/*THESE ARE HARDCODED TO POST TO MY DEV ACCOUNT PAGE ONLY, NEED TO CAPTURE FROM AUTH FLOW*/
 $page_access_token = 'CAACEdEose0cBAJJIvTyqFukvtnojEivaILXG6jWcKxi7K9ipZBp0m9xR1eJ1JeeXpo8OVd97TZCZAMvdiRfaLJsv53RDmuMbQdkTGoTFggqeyqSZCZBtv3yAYHU1h17tIMiAvOrMUU5OaOtggD1b8Jy14ZAH3uFeeGbMu9AEWfQYWSLnPvW9nDcapFhn98Lve9ZBv45DZCdxFJXtwPzxrDmJ';
 $page_id = '1475915989342199';
 
@@ -8,7 +12,7 @@ $data['picture'] = "http://i.imgur.com/l4ZXhsf.jpg"; /*replace with my hosted ve
 $data['link'] = "local.LastTimeUWbeatUO.com";
 $data['message'] = "I was 77 last time UW beat UO.";
 $data['caption'] = "Find out how old you were last time UW beat UO";
-$data['description'] = "Visit our site and share with your friends!";
+$data['description'] = "www.LastTimeUWBeatUO.com";
 
 $data['access_token'] = $page_access_token;
 
