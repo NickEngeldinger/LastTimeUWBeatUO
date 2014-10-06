66f7d20351ad1ce85ab282061f6b016a2f795ace
user <user@lubuntu.(none)>
2014-10-06 17:23:00 -0400

    Grab age from GET var and pass it through as part of FB status post

--- facebook/redirect.php
+++ facebook/redirect.php
@@ -1,18 +1,22 @@
 <?php
 
-$age = $_GET['age'];
-echo $age;
-/*Check for no age and then set up message variable for you weren' even born yet*/
+$age = isset($_GET['age']) ? $_GET['age']: null;
 
-/*THESE ARE HARDCODED TO POST TO MY DEV ACCOUNT PAGE ONLY, NEED TO CAPTURE FROM AUTH FLOW*/
-$page_access_token = 'CAACEdEose0cBAJJIvTyqFukvtnojEivaILXG6jWcKxi7K9ipZBp0m9xR1eJ1JeeXpo8OVd97TZCZAMvdiRfaLJsv53RDmuMbQdkTGoTFggqeyqSZCZBtv3yAYHU1h17tIMiAvOrMUU5OaOtggD1b8Jy14ZAH3uFeeGbMu9AEWfQYWSLnPvW9nDcapFhn98Lve9ZBv45DZCdxFJXtwPzxrDmJ';
+if(age == null)
+	$msg = "You weren't even born the last time UW beat UO!"
+else
+	$msg = "You were ".$age." last time UW beat UO! #GoDucks"
+
+/*THESE ARE HARDCODED TO POST TO MY DEV ACCOUNT PAGE ONLY, NEED TO CAPTURE FROM AUTH FLOW 
+ALSO THE TOKEN WILL EXPIRE AFTER 60MIN AND NEEDS TO BE RE-GENERATED*/
+$page_access_token = 'CAACEdEose0cBAG0ZCUY1X5NZA2QboGUkPI2oILIHz40A39Ey2LGqSO2EexWcZABSrIkciN5saBMZC7780ZAL2xhTZAB64ARtefyFnaZBCRWZAanVCly9R9rtqBq4t0ZCbxC0MlWBGppB3S06x20g0CGlA7FL7szz30nwF0A9ZBl9wI4SsgsQfq0CfCXlZCE0AwkU7aIfGKFfDbd6kWzjuQ7ZC1ku';
 $page_id = '1475915989342199';
 
 $data['picture'] = "http://i.imgur.com/l4ZXhsf.jpg"; /*replace with my hosted version when live */
 $data['link'] = "local.LastTimeUWbeatUO.com";
-$data['message'] = "I was 77 last time UW beat UO.";
-$data['caption'] = "Find out how old you were last time UW beat UO";
-$data['description'] = "www.LastTimeUWBeatUO.com";
+$data['message'] = $msg;
+$data['caption'] = "www.LastTimeUWBeatUO.com";
+$data['description'] = "The Oregon Ducks football team hasn't lost to the Washington Huskies since November 16th, 2002. Calculate how old you were last time UW beat UO and share with your friends!";
 
 $data['access_token'] = $page_access_token;
 
@@ -26,6 +30,6 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $return = curl_exec($ch);
 curl_close($ch);
 
-echo $return;
+/* NEED TO CHECK IF POSTING WAS SUCCESSFUL THEN REDIRECT TO THANKS PAGE, OTHERWISE PRINT ERROR MESSAGE */
 
 ?>
